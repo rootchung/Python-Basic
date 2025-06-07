7c12b7485115229f889fcabce7eadad3c672002c
rootchung <bon.chung.2015@gmail.com>
2025-06-07 05:56:52 +0000

    20250607

---
+++ variable.py
@@ -0,0 +1,58 @@
+# variable
+# variable = value
+# Save the value in variable in the format above.
+# = Unlike the meaning of the symbol, the value is simply stored in a variable.
+# Variable can be whatever you want as long as you followthe rules below.
+# 1. Impossible to start with a number
+# 2. Special characters are not possible except the symbol"_"
+# 3. No spacing
+# 4. Reserved words not possible
+
+# my_name = "bon"
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# Data types
+# str   -> String       => made up of quotes" " ' '
+# float -> Float        => no quotes and has a decimal point
+# int   -> Integer      => no quotes and no decimal point
+# Everyday words (English, Chinese, Korean, etc.) without quotes => error
+
+# a = 10  # saves the integer 10 in the variable a
+# b = 'cit'   # saves the string 'cit' in the variable b
+
+# name = 'Python'
+# age = 20
+# height = 178
+
+# print(name)
+
+# name = 'Pong'
+# print(name)
+
+# name = 'Go!'
+# print(name)
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# print('text' or variable or number)
+# The print() funcion prints the 'text', number, or value of
+# the varaible inside the bracket and then adds a newline.
+# To print multiple with items, use a comma (,)
+# If you use print() with nothing inside, it prints an empty line.
+
+name = 'Python'
+age = 28
+height = 188
+print()     # prints an empty line
+print(age)
+print(height)
+print('hi')
+print()
+print(5)
+print (5*10)
+print(name, age, height, "hello")
